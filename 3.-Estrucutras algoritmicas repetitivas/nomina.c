7a88d5041d5fd001629db46ef7bf15524316d398
Saul Corona <saulcoronasantos@ymail.com>
2020-09-16 23:20:01 -0500

    estructura repetitiva for

---
+++ 3.-Estrucutras algoritmicas repetitivas/nomina.c
@@ -0,0 +1,23 @@
+/* Nómina.
+El programa, al recibir los salarios de 15 profesores, obtiene el total de la
+➥nómina de la universidad.
+I: variable de tipo entero.
+SAL y NOM: variables de tipo real. */
+
+/*estructura repetitiva for*/
+
+#include <stdio.h>
+
+int main(void) {
+  int I;
+  float SAL, NOM;
+  NOM = 0;
+
+  for(I=1; I <= 15; I++){
+    printf("\nIngresa el salario del profesor %d: ",I);
+    scanf("%f", &SAL);
+
+    NOM = NOM + SAL;
+  }
+  printf("\nLa nomina es: %.2f", NOM);
+}
---
+++ 3.-Estrucutras algoritmicas repetitivas/sumaPositivos.c
@@ -0,0 +1,25 @@
+/* programa en C que, al recibir
+como datos N números enteros, obtenga solamente la suma de los números
+positivos. */
+
+/*estructura repetitiva for*/
+
+#include <stdio.h>
+
+int main(void) {
+  int I, SUM,NUM,N;
+
+  SUM = 0;
+
+  printf("\nIngresa el numero de datos: ");
+  scanf("%d",&N);
+
+  for (I=1; I<=N; I++){
+    printf("\nIngresa el dato %d: ", I);
+    scanf("%d", &NUM);
+    if (NUM > 0){
+      SUM = SUM + NUM;
+    }
+  }
+  printf("\nLa suma es %d:\t", SUM);
+}
