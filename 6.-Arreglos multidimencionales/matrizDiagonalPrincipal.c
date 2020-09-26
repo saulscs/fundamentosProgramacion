0d5fbc4424639dab9d038a0a923a36f91ea8bca1
Saul Corona <saulcoronasantos@ymail.com>
2020-09-26 15:10:59 -0500

    6.-Arreglos multidimencionales

---
+++ 6.-Arreglos multidimencionales/matrizDiagonalPrincipal.c
@@ -0,0 +1,34 @@
+/* Diagonal principal.
+El programa, al recibir como dato una matriz de tipo entero, escribe la
+diagonal principal. */
+
+#include <stdio.h>
+
+const int TAM = 10;
+
+void lectura(int  [][TAM], int );
+void imprimir(int  [][TAM], int );
+
+int main() {
+  int MAT[TAM][TAM];
+  lectura(MAT,TAM);
+  imprimir(MAT,TAM);
+}
+
+void lectura(int A[][TAM], int F){
+  int I, J;
+    for (I=0; I < F; I++)
+      for (J=0; I < F; J++){
+        printf("Ingrese el elemento %d %d: ", I+1, J+1);
+        scanf("%d", &A[I][J]);
+      }
+}
+
+void imprimir(int A[][TAM], int F){
+  int I,J;
+  for (I=0; I<F; I++)
+    for (J=0; J< TAM; J++)
+      if(I == J)
+        printf("\nDiagonal %d %d : %d", I,J,A[I][J]);
+    
+}
---
+++ 6.-Arreglos multidimencionales/sumaMatrices.c
@@ -0,0 +1,61 @@
+/* Suma matrices.
+El programa, al recibir como datos dos arreglos bidimensionales del mismo
+➥tamaño, calcula la suma de ambos y la almacena en un tercer arreglo
+➥bidimensional. */
+
+#include <stdio.h>
+
+const int MAX = 5;
+
+void lectura(int [][MAX], int , int );
+void suma(int  [][MAX], int [][MAX], int [][MAX], int , int );
+void imprime (int [][MAX], int , int );
+
+int main(void){
+  int MA[MAX][MAX], MB[MAX][MAX], MC[MAX][MAX];
+  int FIL, COL;
+
+  do {
+    printf("\nIngresa en numero de filas del arreglos: ");
+    scanf("%d",&FIL);
+  }
+  while (FIL > MAX || FIL < 1 );
+
+  do {
+    printf("\nIngresa en numero de columnas del arreglos: ");
+    scanf("%d",&COL);
+  }
+  while (COL > MAX || COL < 1 );
+  printf("\nLectura del arreglo MA\n");
+  lectura(MA,FIL,COL);
+  printf("\nLectura del arreglo MB\n");
+  lectura(MB,FIL,COL);
+  suma (MA,MB,MC,FIL,COL);
+  printf("\nImpresion del arreglo MC\n");
+  imprime(MC,FIL,COL);
+}
+
+void lectura(int A[][MAX], int F, int C){
+  int I,J;
+  for (I=0; I<F; I++)
+    for (J=0; J < C; J++){
+      printf("Ingrese el elemento %d %d: ", I+1, J+1);
+      scanf("%d", &A[I][J]);
+    }
+}
+
+void suma(int M1[][MAX], int M2[][MAX], int M3[][MAX], int F, int C){
+  int I, J;
+  for (I=0; I<F; I++)
+    for(J=0; J<C; J++){
+      M3[I][J] = M1[I][J] + M2[I][J];
+    }
+}
+
+void imprime (int A[][MAX], int F, int C){
+  int I, J;
+  for (I=0; I< F; I++)
+    for (J=0; J<C; I++){
+      printf("\nElemento %d %d: %d", I,J, A[I][J]);
+    }
+}
